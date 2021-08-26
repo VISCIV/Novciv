7c9a65cc06abb2917f284bc29fc841d25cbd3854
VISCIV <visciv.official@gmail.com>
2021-08-26 18:04:27 +0700

    fix menu

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
-$(document).ready(function(){
-  $(".open-menu").click(function(){
-    $(".menu-box").addClass("active");
+$(document).ready(function () {
+  $('.open-menu').click(function () {
+    $('.menu-box').addClass('active');
   });
+  $('.close-button').click(function () {
+    $('.menu-box').removeClass('active');
+  });
+});
+
+window.addEventListener('mouseup', function (event) {
+  var menu = document.querySelector('.menu-box');
+  if (event.target != menu && event.target.parentNode != menu) {
+    menu.classList.remove('active');
+  }
 });

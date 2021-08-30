b8037fe4715c58e35a653e7bf865ef0b43731c77
VISCIV <visciv.official@gmail.com>
2021-08-30 09:03:30 +0700

    new Project

--- js/index.js
+++ js/index.js
@@ -1,19 +1,3 @@
-$(document).ready(function () {
-  $('.open-menu').click(function () {
-    $('.menu-box').addClass('active');
-  });
-  $('.close-button').click(function () {
-    $('.menu-box').removeClass('active');
-  });
-});
-
-window.addEventListener('mouseup', function (event) {
-  var menu = document.querySelector('.menu-box');
-  if (event.target != menu && event.target.parentNode != menu) {
-    menu.classList.remove('active');
-  }
-});
-
 var slideIndex = 1;
 
 var myTimer;
